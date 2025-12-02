79e7958be63004541dd7f186313b9b998dcef422
Mehmet Şakir Şeker <mehmetalmehmet63@gmail.com>
2025-12-02 21:12:28 +0300

    hotfix

--- database.py
+++ database.py
@@ -1,6 +1,17 @@
-import kagglehub
+# This script is used to download the dataset using kagglehub
+# You must run this script once before start working on the data
 
-# Download latest version
+import kagglehub
+import os
+import shutil
+
 path = kagglehub.dataset_download("maharshipandya/-spotify-tracks-dataset")
 
-print("Path to dataset files:", path)
+print(f"Dataset downloaded in cache! Copying into the project folder...\nCache path: {path}")
+
+if not os.path.exists("./datasets"):
+    os.mkdir("./datasets")
+
+shutil.copy2(f"{path}/dataset.csv", "./datasets/dataset.csv")
+
+print("Dataset downloaded successfully!")
